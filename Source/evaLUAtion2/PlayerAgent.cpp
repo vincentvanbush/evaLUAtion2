2b4d9c568513bc820d42cc7230dd59d355963e46
chris710 <krzesimir.samborski@student.put.poznan.pl>
2015-12-05 18:39:42 +0100

    PlayerAgent class added

---
+++ Source/evaLUAtion2/PlayerAgent.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "evaLUAtion2.h"
+#include "PlayerAgent.h"
+
+
+//Action* decideNextAction(){};			//corresponds to whatToDoNow method
+
+void UPlayerAgent::selectWeapon(EWeaponType weapon){};
+
+void UPlayerAgent::moveDirection(FVector direction){};
+
+void UPlayerAgent::moveTo(FVector target){};
+
+void UPlayerAgent::reload(){};
+
+void UPlayerAgent::rotate(FVector direction){};
+
+void UPlayerAgent::shootAt(AEvaCharacter* enemy){};
+
+void UPlayerAgent::shootAtPoint(FVector vect){};
+
+void UPlayerAgent::wait(){};
+
+void UPlayerAgent::continueAction(){};
+
+
---
+++ Source/evaLUAtion2/PlayerAgent.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "Object.h"
+#include "Misc.h"
+#include "Evacharacter.h"
+#include "PlayerAgent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVALUATION2_API UPlayerAgent : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, Category = References)
+		AEvaCharacter* ControlledCharacter;
+	
+	//UFUNCTION(BlueprintCallable, Category = EvaluationActions)		//TODO uncomment after Action class is done
+//		Action* decideNextAction();			//corresponds to whatToDoNow method
+	
+	UFUNCTION(BlueprintCallable, Category = EvaluationActions)
+		void selectWeapon(EWeaponType weapon);
+
+	UFUNCTION(BlueprintCallable, Category = EvaluationActions)
+		void moveDirection(FVector direction);
+
+	UFUNCTION(BlueprintCallable, Category = EvaluationActions)
+		void moveTo(FVector target);
+
+	UFUNCTION(BlueprintCallable, Category = EvaluationActions)
+		void reload();
+
+	UFUNCTION(BlueprintCallable, Category = EvaluationActions)
+		void rotate(FVector direction);
+
+	UFUNCTION(BlueprintCallable, Category = EvaluationActions)
+		void shootAt(AEvaCharacter* enemy);
+
+	UFUNCTION(BlueprintCallable, Category = EvaluationActions)
+		void shootAtPoint(FVector vect);
+
+	UFUNCTION(BlueprintCallable, Category = EvaluationActions)
+		void wait();
+
+	UFUNCTION(BlueprintCallable, Category = EvaluationActions)
+		void continueAction();
+
+protected:
+	//TODO add CurrentActorKnowledge
+	//TODO add NextAction
+
+};
